f3fb35eed4559ff83349ffe775fb05a78196cb51
davidalves1 <alves.david@outlook.com>
2019-02-18 23:24:48 -0300

    Some corrections

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -4,9 +4,7 @@ const store = async function (req, res) {
   const tweet = await Tweet.findById(req.params.id)
 
   tweet.set({ likes:  tweet.likes + 1 })
-
   await tweet.save()
-
   req.io.emit('like', tweet)
 
   return res.json(tweet)
