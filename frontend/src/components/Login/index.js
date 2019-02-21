bbfacc4c1a4d4ed89ae9216ac3539889a0952f80
davidalves1 <alves.david@outlook.com>
2019-02-20 23:57:28 -0300

    Store login data on localstorage

--- frontend/src/components/Login/index.js
+++ frontend/src/components/Login/index.js
@@ -5,14 +5,43 @@ import './style.css';
 import twitterLogo from '../../images/twitter-logo.png';
 
 export default class Login extends Component {
+  state = {
+    'username': '',
+  };
+
+  handleInputChange = (ev) => {
+    console.log(ev.target.value);
+    this.setState({ username: ev.target.value });
+  }
+
+  handleSubmitForm = (ev) => {
+    ev.preventDefault();
+
+    const { username } = this.state;
+
+    if (!username.length) {
+      return;
+    }
+
+    localStorage.setItem('@OmniTwitter:username', username);
+
+    this.props.history.push('/timeline');
+  }
+
   render() {
+    const { username } = this.state;
+    
     return (
       <div className="row">
-        <div className="offset-md-4 col-md-4 text-center" style={{marginTop: '20%'}}>
+        <div className="offset-md-4 col-md-4 login-wrapper">
           <img src={twitterLogo} alt="OmniTwitter" className="twitter-logo" />
-          <form>
+          <form onSubmit={this.handleSubmitForm}>
             <div className="form-group">
-              <input className="form-control" placeholder="username" />
+              <input className="form-control" 
+                placeholder="Username"
+                value={username}
+                onChange={this.handleInputChange}
+              />
               <button className="btn btn-block btn-primary mt-3" type="submit">Login</button>
             </div>
           </form>
